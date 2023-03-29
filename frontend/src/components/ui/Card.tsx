1f8614f877c5cb2a438363c82e6aa5414de24627
Z Ta <zilvistam@gmail.com>
2023-03-29 17:36:29 +0100

    refactor: Card converted to tailwind

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -1,11 +1,9 @@
-import classes from "./Card.module.css";
-
 type Props = {
   children: JSX.Element | JSX.Element[] | string;
 };
 
 const Card = (props: Props) => {
-  return <div className={classes.card}>{props.children}</div>;
+  return <div className="border border-sky-600 rounded-lg p-2">{props.children}</div>;
 };
 
 export default Card;
