766a3e09c4d0195a862618a5ef0564211d5f7960
Z Ta <zilvistam@gmail.com>
2023-04-01 12:37:09 +0100

    refactor: useHttp used by register comp

--- frontend/src/components/user/Register.tsx
+++ frontend/src/components/user/Register.tsx
@@ -1,5 +1,7 @@
 import Button from "components/ui/Button";
-import { useRef, useState } from "react";
+import LoadingSpinner from "components/ui/LoadingSpinner";
+import useHttp from "hooks/useHttp";
+import { useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
@@ -8,7 +10,16 @@ const Register = () => {
   const emailInputRef = useRef<HTMLInputElement>(null);
   const passwordInputRef = useRef<HTMLInputElement>(null);
   const confirmPasswordInputRef = useRef<HTMLInputElement>(null);
+  const { sendRequest, isLoading, data, error } = useHttp();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (error) {
+      setEmailErrorMsg("Please enter valid login credentials");
+    } else if (data) {
+      navigate("/");
+    }
+  }, [data, error, navigate]);
 
   const submitHandler = async (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -31,60 +42,49 @@ const Register = () => {
       password,
     };
 
-    const res = await fetch("http://localhost:8080/register", {
+    sendRequest({
+      url: "http://localhost:8080/register",
       method: "POST",
       body: JSON.stringify(user),
-      headers: {
-        "Content-Type": "application/json",
-      },
+      reqAuth: false,
     });
 
-    const data = await res.json();
-
-    if (res.status === 201) {
-      navigate("/");
-    } else if (res.status === 400) {
-      const param = data.errors[0].param;
-      if (param === "password") {
-        return setPasswordErrorMsg("Please enter matching passwords, min 4 characters");
-      } else if (param === "email") {
-        return setEmailErrorMsg("Please enter a valid email address!");
-      }
-    } else if (res.status === 401) {
-      return setEmailErrorMsg("Email already in use");
-    }
     setEmailErrorMsg("");
     setPasswordErrorMsg("");
   };
 
   return (
     <div className="flex justify-center">
-      <form
-        className="flex flex-col mt-12 items-center w-[11%] border-[1px] border-sky-600 rounded-lg p-4"
-        onSubmit={submitHandler}
-      >
-        <label>E-mail</label>
-        <input className="w-full" ref={emailInputRef} name="email" id="email" type="text" />
-        {emailErrorMsg && <span className="text-[red]">{emailErrorMsg}</span>}
-        <label>Password</label>
-        <input
-          className="w-full"
-          ref={passwordInputRef}
-          name="password"
-          id="password"
-          type="password"
-        />
-        <label className="text-center">Confirm Password</label>
-        <input
-          className="w-full"
-          ref={confirmPasswordInputRef}
-          name="confirm-password"
-          id="confirm-password"
-          type="password"
-        />
-        {passwordErrorMsg && <span className="text-[red]">{passwordErrorMsg}</span>}
-        <Button type="submit">Register</Button>
-      </form>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <form
+          className="flex flex-col mt-12 items-center w-[11%] border-[1px] border-sky-600 rounded-lg p-4"
+          onSubmit={submitHandler}
+        >
+          <label>E-mail</label>
+          <input className="w-full" ref={emailInputRef} name="email" id="email" type="text" />
+          {emailErrorMsg && <span className="text-[red]">{emailErrorMsg}</span>}
+          <label>Password</label>
+          <input
+            className="w-full"
+            ref={passwordInputRef}
+            name="password"
+            id="password"
+            type="password"
+          />
+          <label className="text-center">Confirm Password</label>
+          <input
+            className="w-full"
+            ref={confirmPasswordInputRef}
+            name="confirm-password"
+            id="confirm-password"
+            type="password"
+          />
+          {passwordErrorMsg && <span className="text-[red]">{passwordErrorMsg}</span>}
+          <Button type="submit">Register</Button>
+        </form>
+      )}
     </div>
   );
 };
